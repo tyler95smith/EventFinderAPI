b871c7ad38d9ed25a8f6121fc8c8599659d8be22
Tyler J. Smith <tyler95smith@gmail.com>
2018-03-01 02:08:29 +0000

    in settings.py added the API app to the INSTALLED_APPS. The website would not run and was getting errors from the Conversation and Message models I initialized an Account model to fix one issue and added the releated_name field to the host and guest attributes of the Conversation model i'm not sure what the releated name is for but it did fix the error message. I also initialized the Event model

--- API/models.py
+++ API/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+	eventname = models.CharField(max_length=100)
+
+class Event(models.Model):
+	eventname = models.CharField(max_length=100)
+
 class Conversation(models.Model):
     event = models.ForeignKey(Event,   on_delete=models.CASCADE)
-    host  = models.ForeignKey(Account, on_delete=models.CASCADE)
-    guest = models.ForeignKey(Account, on_delete=models.CASCADE)
+    host  = models.ForeignKey(Account, on_delete=models.CASCADE,related_name="host_account")
+    guest = models.ForeignKey(Account, on_delete=models.CASCADE,related_name="guest_account")
 
 class Message(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
     sender = models.ForeignKey(Account, on_delete=models.CASCADE)
-    message = models.CharField(512)
+    message = models.CharField(max_length=512)
