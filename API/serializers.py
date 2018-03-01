64fffdcd668f9b60564e988b681386984457b875
Taylor Tyger <taylortyger@gmail.com>
2018-03-01 23:17:21 +0000

    rest api: Added serializers for User and Person models

---
+++ API/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from models.Person import Person
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperLinkedModelSerializer):
+	class Meta:
+		model = User
+		fields = ('id','username','email')
+
+class PersonSerializer(serializers.HyperLinkedModelSerializer):
+	class Meta:
+		model = Person
+		fields = ('id', 'user', 'date_of_birth', 'bio', 'primaryLocation', 'currentLocation', 'hideLocation')
