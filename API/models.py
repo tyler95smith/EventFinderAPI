996a3770203d891fce856b5865797995c5bc617f
Tyler J. Smith <tyler95smith@gmail.com>
2018-03-01 01:31:28 +0000

    the server wasn't starting properly and i was unable to create apps, i think because of the 'model' files that were directly in the project folder. I deleted them and we didnt have a django app yet so i created an app and moved all the models that were commited at the time from their own files into the models.py of the API app i created

--- API/models.py
+++ API/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# Create your models here.
 class Conversation(models.Model):
     event = models.ForeignKey(Event,   on_delete=models.CASCADE)
     host  = models.ForeignKey(Account, on_delete=models.CASCADE)
@@ -10,4 +11,3 @@ class Message(models.Model):
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
     sender = models.ForeignKey(Account, on_delete=models.CASCADE)
     message = models.CharField(512)
-    
