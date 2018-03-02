25b04c4eb6dfb56af8a7a331d9f067ba91fc2af4
Tyler J. Smith <tyler95smith@gmail.com>
2018-03-02 02:25:37 +0000

    made some of the models modifiable through the built in admin inteface

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Conversation,Event,Interests,Message,Notification,Person,Report
+
 # Register your models here.
+
+admin.site.register(Conversation)
+admin.site.register(Event)
+admin.site.register(Interests)
+admin.site.register(Message)
+admin.site.register(Notification)
+admin.site.register(Person)
+admin.site.register(Report)
+
