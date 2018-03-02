ee2bfe03ae4b79caf7557fa5b8313eefe2b38bdb
Taylor Tyger <taylortyger@gmail.com>
2018-03-02 00:44:13 +0000

    Added create method to UserSerializer

--- API/serializers.py
+++ API/serializers.py
@@ -3,9 +3,25 @@ from models.Person import Person
 from rest_framework import serializers
 
 class UserSerializer(serializers.HyperLinkedModelSerializer):
+
+	password = serializers.CharField(write_only=True)
+
+	def create(self, valid_data):
+
+		user = User.objects.create(
+				username=valid_data['username'],
+				email=valid_data['email']
+			)
+		user.set_password(valid_data['password'])
+		user.save()
+
+		return user
+
 	class Meta:
 		model = User
 		fields = ('id','username','email')
+
+
 
 class PersonSerializer(serializers.HyperLinkedModelSerializer):
 	user = UserSerializer()
