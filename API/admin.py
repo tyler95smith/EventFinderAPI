d701bdc879c8b08ecb232fd51f7bce1675fcdf1e
Taylor Tyger <taylortyger@gmail.com>
2018-04-25 23:19:20 +0000

    Added Rsvp to django admin

--- API/admin.py
+++ API/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Conversation,Event,Interests,Message,Notification,Person,Report
+from .models import Conversation,Event,Interests,Message,Notification,Person,Report,Rsvp
 
 # Register your models here.
 
@@ -11,4 +11,5 @@ admin.site.register(Message)
 admin.site.register(Notification)
 admin.site.register(Person)
 admin.site.register(Report)
+admin.site.register(Rsvp)
 
